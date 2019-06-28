8eaed879b8e87058687493f65cedc350d557807f
Ramesh Rajagopalan <ramesh.kumar.rajagopalan@monsanto.com>
2019-06-28 09:32:37 -0500

    pass back parent row to calculations

--- example/demo.jsx
+++ example/demo.jsx
@@ -9,18 +9,18 @@ var gh = require('./gh.jsx')
 var data = require('./data.json')
 
 var dimensions = [
-  {value: 'firstName', title: 'First Name'},
-  {value: 'lastName', title: 'Last Name'},
-  {value: 'state', title: 'State'},
+  { value: 'firstName', title: 'First Name' },
+  { value: 'lastName', title: 'Last Name' },
+  { value: 'state', title: 'State' },
   {value: function(row) {
-    return row.transaction.business
+      return row.transaction.business
   }, title: 'Business'},
   {value: function(row) {
-    return row.transaction.type
+      return row.transaction.type
   }, title: 'Transaction Type'}
 ]
 
-var reduce = function(row, memo) {
+var reduce = function (row, memo) {
   memo.count = (memo.count || 0) + 1
   memo.amountTotal = (memo.amountTotal || 0) + parseFloat(row.transaction.amount)
   return memo
@@ -43,10 +43,10 @@ var calculations = [
   },
   {
     title: 'Avg Amount',
-    value: function(row) {
+    value: function (row) {
       return row.amountTotal / row.count
     },
-    template: function(val, row) {
+    template: function (val, row) {
       return '$' + val.toFixed(2)
     },
     className: 'alignRight'
@@ -54,14 +54,14 @@ var calculations = [
 ]
 
 var Demo = createReactClass({
-  getInitialState: function() {
-    return {showInput: false}
+  getInitialState: function () {
+    return { showInput: false }
   },
-  toggleShow: function() {
+  toggleShow: function () {
     var showInput = this.state.showInput
-    this.setState({showInput: !showInput})
+    this.setState({ showInput: !showInput })
   },
-  render: function() {
+  render: function () {
     return (
       <div className='demo'>
         <h1>ReactPivot</h1>
--- lib/pivot-table.jsx
+++ lib/pivot-table.jsx
@@ -25,7 +25,7 @@ module.exports = createReactClass({
     }
   },
 
-  render: function() {
+  render: function () {
     var results = this.props.rows
 
     var paginatedResults = this.paginate(results)
@@ -45,97 +45,100 @@ module.exports = createReactClass({
     )
   },
 
-  renderTableHead: function(columns) {
+  renderTableHead: function (columns) {
     var self = this
     var sortBy = this.props.sortBy
-    var sortDir =  this.props.sortDir
+    var sortDir = this.props.sortDir
 
     return (
       <thead>
-        <tr>
-          { columns.map(function(col) {
-            var className = col.className
-            if (col.title === sortBy) className += ' ' + sortDir
+      <tr>
+        {columns.map(function (col) {
+          var className = col.className
+          if (col.title === sortBy) className += ' ' + sortDir
 
-            var hide = ''
-            if (col.type !== 'dimension') hide = (
-              <span className='reactPivot-hideColumn'
-                    onClick={partial(self.props.onColumnHide, col.title)}>
+          var hide = ''
+          if (col.type !== 'dimension') hide = (
+            <span className='reactPivot-hideColumn'
+                  onClick={partial(self.props.onColumnHide, col.title)}>
                 &times;
               </span>
-            )
+          )
 
-            return (
-              <th className={className}
-                  onClick={partial(self.props.onSort, col.title)}
-                  style={{cursor: 'pointer'}}
-                  key={col.title}>
+          return (
+            <th className={className}
+                onClick={partial(self.props.onSort, col.title)}
+                style={{ cursor: 'pointer' }}
+                key={col.title}>
 
-                {hide}
-                {col.title}
-              </th>
-            )
-          })}
-        </tr>
+              {hide}
+              {col.title}
+            </th>
+          )
+        })}
+      </tr>
       </thead>
     )
   },
 
-  renderTableBody: function(columns, rows) {
+  renderTableBody: function (columns, rows) {
     var self = this
-
+    let parentRow = rows[0]
     return (
       <tbody>
-        {rows.map(function(row) {
-          return (
-            <tr key={row._key} className={"reactPivot-level-" + row._level}>
-              {columns.map(function(col, i) {
-                if (i < row._level) return <td key={i} className='reactPivot-indent' />
+      {rows.map(function (row) {
+        if (row._level - parentRow._level !== 1) {
+          parentRow = row
+        }
+        return (
+          <tr key={row._key} className={"reactPivot-level-" + row._level}>
+            {columns.map(function (col, i) {
+              if (i < row._level) return <td key={i} className='reactPivot-indent' />
 
-                return self.renderCell(col, row)
-              })}
-            </tr>
-          )
+              return self.renderCell(col, row, parentRow)
+            })}
+          </tr>
+        )
 
-        })}
+      })}
       </tbody>
     )
   },
 
-  renderCell: function(col, row) {
+  renderCell: function (col, row, parentRow) {
     if (col.type === 'dimension') {
       var val = row[col.title]
       var text = val
       var dimensionExists = (typeof val) !== 'undefined'
-      if (col.template && dimensionExists) text = col.template(val, row)
+      if (col.template && dimensionExists) text = col.template(val, row, parentRow)
     } else {
       var val = getValue(col, row)
       var text = val
-      if (col.template) text = col.template(val, row)
+      if (col.template) text = col.template(val, row, parentRow)
     }
 
     if (dimensionExists) {
       var solo = (
         <span className='reactPivot-solo'>
-          <a style={{cursor: 'pointer'}}
+          <a style={{ cursor: 'pointer' }}
              onClick={partial(this.props.onSolo, {
-                title: col.title,
-                value: val
-              })}>{this.props.soloText}</a>
+               title: col.title,
+               value: val
+             })}>{this.props.soloText}</a>
         </span>
       )
     }
 
-    return(
+    return (
       <td className={col.className}
           key={[col.title, row.key].join('\xff')}
           title={col.title}>
-        <span dangerouslySetInnerHTML={{__html: text || ''}}></span> {solo}
+        <span dangerouslySetInnerHTML={{ __html: text || '' }}></span> {solo}
       </td>
     )
   },
 
-  renderPagination: function(pagination) {
+  renderPagination: function (pagination) {
     var self = this
     var nPaginatePages = pagination.nPages
     var paginatePage = pagination.curPage
@@ -144,12 +147,12 @@ module.exports = createReactClass({
 
     return (
       <div className='reactPivot-paginate'>
-        {_.range(0, nPaginatePages).map(function(n) {
+        {_.range(0, nPaginatePages).map(function (n) {
           var c = 'reactPivot-pageNumber'
           if (n === paginatePage) c += ' is-selected'
           return (
             <span className={c} key={n}>
-              <a onClick={partial(self.setPaginatePage, n)}>{n+1}</a>
+              <a onClick={partial(self.setPaginatePage, n)}>{n + 1}</a>
             </span>
           )
         })}
@@ -157,8 +160,8 @@ module.exports = createReactClass({
     )
   },
 
-  paginate: function(results) {
-    if (results.length <= 0) return {rows: results, nPages: 1, curPage: 0}
+  paginate: function (results) {
+    if (results.length <= 0) return { rows: results, nPages: 1, curPage: 0 }
 
     var paginatePage = this.state.paginatePage
     var nPaginateRows = this.props.nPaginateRows
@@ -172,7 +175,7 @@ module.exports = createReactClass({
     var boundaryLevel = results[iBoundaryRow]._level
     var parentRows = []
     if (boundaryLevel > 0) {
-      for (var i = iBoundaryRow-1; i >= 0; i--) {
+      for (var i = iBoundaryRow - 1; i >= 0; i--) {
         if (results[i]._level < boundaryLevel) {
           parentRows.unshift(results[i])
           boundaryLevel = results[i]._level
@@ -184,11 +187,11 @@ module.exports = createReactClass({
     var iEnd = iBoundaryRow + nPaginateRows
     var rows = parentRows.concat(results.slice(iBoundaryRow, iEnd))
 
-    return {rows: rows, nPages: nPaginatePages, curPage: paginatePage}
+    return { rows: rows, nPages: nPaginatePages, curPage: paginatePage }
   },
 
-  setPaginatePage: function(nPage) {
-    this.setState({paginatePage: nPage})
+  setPaginatePage: function (nPage) {
+    this.setState({ paginatePage: nPage })
   }
 })
 
